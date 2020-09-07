9307dd8367d44e8ceaed2093007e7a2e0b8b2410
ShokoLocoMocco <stachikawa@gradcenter.cuny.edu>
2020-09-07 00:06:56 -0400

    completed tutorial 0

---
+++ tutorial0_serve/main.js
@@ -0,0 +1,5 @@
+console.log('konnichiwa!')
+console.log(5*5)
+console.log('chicken or beef?')
+alert('Has anyone told u today how awesome Shoko is?')
+alert('Jk, YOU are awesome! Have a great day!')
